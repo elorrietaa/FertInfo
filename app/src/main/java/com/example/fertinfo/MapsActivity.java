d292675e3c6758b5195f91a6fba37e1a829f90ee
sara <s.hernandez@ibermatica.con>
2020-02-25 12:30:09 +0100

    texto asistente

--- app/src/main/java/com/example/fertinfo/MapsActivity.java
+++ app/src/main/java/com/example/fertinfo/MapsActivity.java
@@ -91,7 +91,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                                     sydney = new LatLng(lat, lon);
                                     mMap.addMarker(new MarkerOptions().position(sydney).title("My Ubicación").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
                                     mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 8.0f));
-                                    // mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, zoom));
+                                     mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, zoom));
 
 
 
@@ -211,6 +211,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 mMap.addMarker(new MarkerOptions().position(sydney).title(name));
                 mMap.setMapType(mMap.MAP_TYPE_NORMAL);
+
 
 
 
--- app/src/main/java/com/example/fertinfo/MyAdapterAnalisis.java
+++ app/src/main/java/com/example/fertinfo/MyAdapterAnalisis.java
@@ -80,7 +80,7 @@ public class MyAdapterAnalisis extends RecyclerView.Adapter<MyAdapterAnalisis.My
                         .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                Log.d("MainActivity", "Sending atomic bombs to Jupiter");
+
 
                             }
                         })
