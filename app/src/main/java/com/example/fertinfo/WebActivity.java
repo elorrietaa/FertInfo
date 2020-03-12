db5071763bdcc15598ade727c1766718ca9f2f05
sara <s.hernandez@ibermatica.con>
2020-03-12 10:57:53 +0100

    flecha para atras en webActivity

--- app/src/main/java/com/example/fertinfo/WebActivity.java
+++ app/src/main/java/com/example/fertinfo/WebActivity.java
@@ -2,7 +2,9 @@ package com.example.fertinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -13,6 +15,8 @@ public class WebActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Pedir cita");
 
          myWebView = (WebView) findViewById(R.id.webview);
         //final WebSettings ajustesVisorWeb = myWebView.getSettings();
@@ -20,5 +24,15 @@ public class WebActivity extends AppCompatActivity {
 
 
         myWebView.loadUrl(url);
+    }public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            Intent i = new Intent(this,AnalisisActivity.class);
+            startActivity(i);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
+
 }
